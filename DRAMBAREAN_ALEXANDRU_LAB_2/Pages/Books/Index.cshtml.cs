bfa2160da63bd4f9df7df63cbd5827366b09fa64
drambreanalexanru <alexandru.drambarean@studd.ubbcluj.ro>
2022-10-26 21:06:15 +0300

    Add project files.

---
+++ DRAMBAREAN_ALEXANDRU_LAB_2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DRAMBAREAN_ALEXANDRU_LAB_2.Data;
+using DRAMBAREAN_ALEXANDRU_LAB_2.Models;
+
+namespace DRAMBAREAN_ALEXANDRU_LAB_2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DRAMBAREAN_ALEXANDRU_LAB_2.Data.DRAMBAREAN_ALEXANDRU_LAB_2Context _context;
+
+        public IndexModel(DRAMBAREAN_ALEXANDRU_LAB_2.Data.DRAMBAREAN_ALEXANDRU_LAB_2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book
+                    .Include(b => b.Publisher)
+                    .ToListAsync();
+            }
+        }
+    }
+}
